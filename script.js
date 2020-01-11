aeaba0bb417f2e9da65777b3a6e083ccac29976f
Randall Phillips <rphill45@uncc.edu>
2020-01-11 15:15:07 -0500

    designed hour blocks and used moment to display current date

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var timeSlot = document.querySelector("#times");
+$("#currentDate").text(moment().format("dddd, MMMM Do"));
 
-timeSlot.addEventListener("click",(event)=> {
-    event.preventDefault();
-    alert("boo");
-});
+display = () => {
+
+    var currentBGTime = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm", "5pm"];
+
+    var currentBGHours = [9,10,11,12,13,14,15,16,17];
+
+    var currentTime = moment();
+}
